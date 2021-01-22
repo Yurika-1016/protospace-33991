31de18f26861a85d05e5cc7173d4af1827ce6bcc
Yurika-1016 <cix11103@nitech.jp>
2021-01-22 18:09:31 +0900

    second commit2

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   validates :profile, presence: true
   validates :occupation, presence: true
   validates :position, presence: true
+  validates :password, confirmation: true
 
   has_many :prototypes, dependent: :destroy
   has_many :comments, dependent: :destroy
